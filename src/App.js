a144541dd0be80bf9990b8794d10a83e712f2022
Ranita Layek <ranita.layek92@gmail.com>
2022-04-20 12:06:33 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import PostList from './Components/PostList';
+import PostForm from './Components/PostForm';
 
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				{/*<PostForm />*/}
-				<PostList /> 
+				<PostForm />
+				{/*<PostList /> */}
 			</div>
 		)
 	}
